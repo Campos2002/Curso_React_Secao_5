0dd77f9bf33d99622b593e60adf6c0a27ff01886
Campos2002 <campereira91@gmail.com>
2022-11-09 02:24:47 -0300

    Curso React - Seção 5 - Matheus Battisti

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -4,9 +4,10 @@ import './MyForm.css'
 const MyForm = ({ user }) => {
 
   //GERENCIAMENTO DE DADOS
+  const [msg, setMsg] = useState(user ? user.msg : '');
   const [name, setName] = useState(user ? user.name : '');
   const [email, setEmail] = useState(user ? user.email : '');
-  const [msg, setMsg] = useState('');
+  const [role, setRole] = useState(user ? user.role : '');
 
   const handleName = (e) => {
     setName(e.target.value);
@@ -17,7 +18,7 @@ const MyForm = ({ user }) => {
     //PREVENÇÃO DE ENVIO
     e.preventDefault();
     console.log('Enviando formulário...');
-    console.log(name, email, msg);
+    console.log(name, email, role, msg);
     //RESET DO FORM
     setName('');
     setEmail('');
@@ -29,15 +30,26 @@ const MyForm = ({ user }) => {
       <form className='container' onSubmit={handleSubmit}>
         <h2 className='title'>Envio de Formulário</h2>
         <div>
-          <label> {/* INPUT TEXTAREA */}
-            <span>Mensagem:</span>
-            <textarea name="mensagem" placeholder='Deixe sua mensagem' onChange={(e) => setMsg(e.target.value)} value={msg}></textarea>
+          <label>
           </label>
           <label>
+            {/* TEXTAREA */}
+            <span>Mensagem:</span>
+            <textarea name="mensagem" placeholder='Deixe sua mensagem' onChange={(e) => setMsg(e.target.value)} value={msg}></textarea>
+            {/* TEXT */}
             <span>Nome:</span>
             <input type="text" name='name' placeholder='Digite seu nome' onChange={handleName} value={name} />
+            {/* EMAIL */}
             <span>E-mail</span>
             <input type="email" name='email' placeholder='Digite seu e-mail' onChange={(e) => setEmail(e.target.value)} value={email} />
+            {/* SELECT */}
+            <span>Função no sistema:</span>
+            <select name="role" onChange={(e) => setRole(e.target.value)} value={role}>
+              <option value="user">Usuário</option>
+              <option value="editor">Editor</option>
+              <option value="admin">Administrador</option>
+            </select>
+            {/* SUBMIT */}
             <input type='submit' value='Enviar' />
           </label>
         </div>
