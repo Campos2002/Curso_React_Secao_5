c4a929c7450b3c9a01a19419caa4bf1ca0276bf5
Campos2002 <campereira91@gmail.com>
2022-11-09 01:08:24 -0300

    Curso React - Seção 5 - Matheus Battisti

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -11,46 +11,69 @@ const MyForm = () => {
     setName(e.target.value);
   };
 
-  console.log(name);
-  console.log(email);
+  /*   console.log(name);
+    console.log(email); */
+
+  // << 5- Envio de Formulário
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Enviando formulário");
+    console.log(name, email)
+  };
 
   return (
     <div>
-      {/* 1 - CRIAÇÃO DE FORM */}
 
-      <form>
-        <h2 className='melecura'>Criação do Formulário</h2>
+      {/* 1 - CRIAÇÃO DE FORM */}
+      <form className='container'>
+        <h2 className='title'>Criação do Formulário</h2>
         <div>
           <label htmlFor="name">Nome:</label>
           <input type="text" name='name' placeholder='Digite seu nome' />
           <input type='submit' value='Enviar' />
         </div>
+
         {/* 2 - LABEL ENVOLVENDO INPUT */}
-        <h2 className='melecura'>Label Envolvendo Input</h2>
+        <h2 className='title'>Label Envolvendo Input</h2>
         <label>
           <span>E-mail</span>
           <input type="email" name='email' placeholder='Digite seu e-mail' />
         </label>
         <input type='submit' value='Enviar' />
+
         {/* 3 - GERENCIAMENTO DE DADOS */}
-        <h2 className='melecura'>Gerenciamento de Dados</h2>
+        <h2 className='title'>Gerenciamento de Dados</h2>
         <div>
           <label htmlFor="name">Nome:</label>
           <input type="text" name='name' placeholder='Digite seu nome' onChange={handleName} />
           <input type='submit' value='Enviar' />
         </div>
+
         {/* 4 - SIMPLIFICANDO A MANIPULAÇÃO DE STATE */}
-        <h2 className='melecura'>Simplificando a Manipulação de State</h2>
+        <h2 className='title'>Simplificando a Manipulação de State</h2>
         <label>
           <span>E-mail</span>
           <input type="email" name='email' placeholder='Digite seu e-mail' onChange={(e) => setEmail(e.target.value)} />
           <input type='submit' value='Enviar' />
         </label>
-        {/* ENVIO DE FORM */}^
-        <h2 className='melecura'>eu amo o gui</h2>
       </form>
+
+      {/* 5 - ENVIO DE FORM */}
+      <form className='container' onSubmit={handleSubmit}>
+        <h2 className='title'>Envio de Form</h2>
+        <div>
+          <label>
+            <span>Nome:</span>
+            <input type="text" name='name' placeholder='Digite seu nome' onChange={handleName} />
+            <span>E-mail</span>
+            <input type="email" name='email' placeholder='Digite seu e-mail' onChange={(e) => setEmail(e.target.value)} />
+            <input type='submit' value='Enviar' />
+          </label>
+        </div>
+      </form>
+
     </div>
   )
-}
+};
 
 export default MyForm
